1a4375ffce956d60d5c97c516c278e0770c8383f
ghemsley <ghemsley@protonmail.ch>
2021-05-17 17:26:45 -0500

    commit tree 97ea3a8d73eecb13ac5f360b45a5aaaa0fa4f69b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const findMatching = (drivers, string) =>
+  drivers.filter((driver) => driver.toLowerCase() === string.toLowerCase());
+
+const fuzzyMatch = (drivers, string) =>
+  drivers.filter(
+    (driver) =>
+      driver.slice(0, string.length).toLowerCase() === string.toLowerCase()
+  );
+
+const matchName = (drivers, string) =>
+  drivers.filter(
+    (driver) => driver.name.toLowerCase() === string.toLowerCase()
+  )
